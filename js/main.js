e6d1f8ceeb4957febdecf6293dd0f4352b1b8d4b
adel <110720004+Adel-elbamby@users.noreply.github.com>
2023-04-26 02:57:23 +0200

    mobile phone colored

---
+++ js/main.js
@@ -0,0 +1,11 @@
+
+let mobile = document.querySelector("#mobile");
+let bod = document.querySelector("#body");
+function phones (phone) {
+    mobile.src = phone;
+
+}
+
+function colors (color){
+    bod.style.background = color;
+}
